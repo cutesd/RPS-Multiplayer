1fc83e93769742efaa882852945222a9e676353e
cutesd <kimberly@kimberlycooper.me>
2018-06-27 20:11:22 -0700

    got players to show up and remove correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,19 @@ $(document).ready(function () {
     var RPSMultiPlayer = function () {
 
         var database, connectionsRef, connectedRef;
+        var uid, cid;
         var win_arr = new Array(2);
         var loss_arr = new Array(2);
-        var playerNum = -1;
+        var numPlayers = 0;
+        var setUp = true;
         var screenName = "";
         var challengeName = "";
         var winCnt = 0;
         var lossCnt = 0;
+        var gameNum = -1;
         var obj = {};
+        var disObj = {};
+        var oL = 0;
 
         this.play = init;
 
@@ -35,68 +40,137 @@ $(document).ready(function () {
         database = firebase.database();
         connectionsRef = database.ref("/connections");
         connectedRef = database.ref(".info/connected");
+        // userRef = database.ref('/presence/' + userid);
+
+        // userRef.on("value", function (snapshot) {
+        //     console.log("user:", snapshot.val);
+        // });
 
         // When the client's connection state changes...
         connectedRef.on("value", function (snap) {
 
             // If they are connected..
             if (snap.val()) {
-
-                // Add user to the connections list.
+                // Add user to the connections list.    
                 var con = connectionsRef.push(true);
                 // Remove user from the connection list when they disconnect.
-                // con.onDisconnect().
                 con.onDisconnect().remove();
+                //lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
+                database.ref("/players").onDisconnect().set(disObj);
             }
         });
 
+        connectionsRef.limitToLast(1).once("value", function (snap) {
+            $.each(snap.val(), function (key, val) {
+                console.log(key, ":", val);
+                uid = key;
+                return false;
+            });
+        });
+
         // When first loaded or when the connections list changes...
-        connectionsRef.limitToLast(2).on("value", function (snap) {
+        connectionsRef.on("value", function (snap) {
 
             // Set playerNum to numChildren
-            console.log(snap.val());
-            // if (playerNum === -1) {
-            //     playerNum = snap.numChildren();
-            // }
-            console.log("num:", playerNum);
-            // playerName = "player"+playerNum;
+            console.log("connections:", snap.val());
+            oL = Object.keys(obj).length;
+            if (oL !== snap.numChildren()) {
+
+                if (oL > snap.numChildren()) {
+                    $.each(obj, function (key, val) {
+                        if (!snap.child(key).exists()) {
+                            delete obj[key];
+
+                            database.ref("/players").set(obj);
+                            return false;
+                        }
+                    });
+                }
+            }
+            $.each(snap.val(), function (key, val) {
+                disObj[key] = obj[key];
+            });
+            oL = Object.keys(obj).length;
+            if (oL === 1) disObj = {};
+            console.log(oL, disObj);
+            if (snap.numChildren() === 2) {
+
+            }
+
         });
 
         /* ====================  Player Setup  ======================== */
-        $("#submitName").on("click", setData);
+        $("#submitName").on("click", setScreenName);
         $("#submitWin").on("click", setWin);
         $("#submitLoss").on("click", setLoss);
 
-        function setData(e) {
+        function setObj(key) {
+            obj[key] = {
+                name: '',
+                win: 0,
+                loss: 0,
+                uid: key
+            };
+            // console.log("obj:", obj);
+            database.ref("/players").set(obj);
+        }
+
+        function setScreenName(e) {
             e.preventDefault();
             screenName = $("#input-name").val();
+            obj[uid].name = screenName;
+            $("#input-name").val('');
+            // console.log("setScreenName:", obj);
+            database.ref("/players").set(obj);
+            setPlayerDisplay();
+        }
+
+        function setChallenger() {
             $.each(obj, function (key, val) {
-                if(key !== screenName){
-                    challengeName = key;
-                    console.log("challenger:", challengeName);
+                if (key !== uid) {
+                    cid = key;
+                    console.log("challenger:", cid);
                     return false;
                 }
             });
-            obj[screenName] = {
-                name: screenName,
-                win: 0,
-                loss: 0
-            };
-            console.log("setData:", obj);
-            database.ref("/players").set(obj);
-            setScreenName();
+            challengeName = obj[cid].name;
+            console.log(obj[cid]);
+            setPlayerDisplay();
         }
 
-        function setScreenName() {
-            $("#myData").text(screenName);
-            $("#challengeData").text(challengeName);
-            $("#input-name").val('');
+        database.ref("/players").limitToLast(2).on("value", function (snapshot) {
+
+            console.log("players:", snapshot.val());
+
+            if (setUp) {
+                setUp = false;
+                if (snapshot.val() !== null) {
+                    obj = snapshot.val();
+                }
+                setObj(uid);
+                oL = Object.keys(obj).length;
+                if (oL > 1) {
+                    setChallenger();
+                }
+            } else {
+                obj = snapshot.val();
+            }
+
+            setPlayerDisplay();
+
+        });
+
+
+        function setPlayerDisplay() {
+            (screenName.length > 0) ? $("#player").text(screenName) : $("#player").text('');
+            (oL > 1 && challengeName.length > 0) ? $("#challenger").text(challengeName) : $("#challenger").text('');
+            
         }
 
 
         /* ====================  WINS & LOSSES  ======================== */
 
-        
+
 
         function setWin(e) {
             e.preventDefault();
@@ -112,37 +186,33 @@ $(document).ready(function () {
             database.ref("/players").set(obj);
         }
 
-        database.ref("/players").limitToLast(2).on("value", function (snapshot) {
-            console.log("players:", snapshot.val());
-            obj = snapshot.val();
-        });
 
-        database.ref("/win").on("value", function (snapshot) {
+        // database.ref("/win").on("value", function (snapshot) {
 
-            // Print the local data to the console.
-            console.log(snapshot.val());
-            if (snapshot.child('arr').exists()) {
-                win_arr = JSON.parse(snapshot.val().arr);
-            }
+        //     // Print the local data to the console.
+        //     console.log(snapshot.val());
+        //     if (snapshot.child('arr').exists()) {
+        //         win_arr = JSON.parse(snapshot.val().arr);
+        //     }
 
 
-            // If any errors are experienced, log them to console.
-        }, function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-        });
+        //     // If any errors are experienced, log them to console.
+        // }, function (errorObject) {
+        //     console.log("The read failed: " + errorObject.code);
+        // });
 
-        database.ref("/loss").on("value", function (snapshot) {
+        // database.ref("/loss").on("value", function (snapshot) {
 
-            // Print the local data to the console.
-            console.log(snapshot.val());
-            if (snapshot.child('arr').exists()) {
-                loss_arr = JSON.parse(snapshot.val().arr);
-            }
+        //     // Print the local data to the console.
+        //     console.log(snapshot.val());
+        //     if (snapshot.child('arr').exists()) {
+        //         loss_arr = JSON.parse(snapshot.val().arr);
+        //     }
 
-            // If any errors are experienced, log them to console.
-        }, function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-        });
+        //     // If any errors are experienced, log them to console.
+        // }, function (errorObject) {
+        //     console.log("The read failed: " + errorObject.code);
+        // });
 
         /* ====================  CHAT  ======================== */
 
